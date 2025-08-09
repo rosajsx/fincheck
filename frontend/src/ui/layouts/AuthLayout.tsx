e3092ff1f285ad9557a5792ded3cc6af9a5a851b
Lucas Rosa <lucassantosrosa51@gmail.com>
2025-08-09 10:45:21 -0300

    feat: update layout and assets for Auth page
    - Enhanced index.html with full height and background color.
    - Added new illustration image for the Auth layout.
    - Introduced a new logo SVG asset.
    - Removed unused React logo SVG.
    - Expanded CSS variables for additional color themes.
    - Created Logo component to encapsulate logo rendering.
    - Updated AuthLayout to include illustration and improved layout structure.

--- frontend/src/ui/layouts/AuthLayout.tsx
+++ frontend/src/ui/layouts/AuthLayout.tsx
@@ -1,10 +1,29 @@
 import { Outlet } from "react-router-dom";
+import illustration from "../../assets/illustration.png";
+import { Logo } from "../components/Logo";
 
 export function AuthLayout() {
   return (
-    <div>
-      <h1>Auth Layout</h1>
-      <Outlet />
+    <div className="flex w-full h-full">
+      <div className=" w-full h-full flex items-center justify-center flex-col gap-16 lg:w-1/2">
+        <Logo className="h-6 text-gray-500" />
+        <div className="  w-full max-w-[504px] px-8">
+          <Outlet />
+        </div>
+      </div>
+      <div className="w-1/2  h-full p-8    justify-center items-center relative hidden lg:flex">
+        <img
+          src={illustration}
+          className=" object-cover w-full h-full max-w-[656px] max-h-[960px] select-none rounded-4xl"
+        />
+        <div className="max-w-[656px] bottom-8  p-10 absolute bg-white rounded-b-4xl ">
+          <Logo className="text-teal-900 h-8" />
+          <p className="text-gray-700 font-medium text-xl mt-6">
+            Gerencie suas finanças pessoais de uma forma simples com o fincheck,
+            e o melhor, totalmente de graça!
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
